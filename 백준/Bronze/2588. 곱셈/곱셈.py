537f17e8e279322c479a73ba8da9b528a8575fad
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-04 16:40:45 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,14 @@
+A = input()
+B = input()
+
+rst_1 = int(A) * int(B[2])
+rst_2 = int(A) * int(B[1])
+rst_3 = int(A) * int(B[0])
+print(rst_1)
+print(rst_2)
+print(rst_3)
+
+print(rst_1 + rst_2 * 10 + rst_3 * 100)
+
+
+
