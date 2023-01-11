680c459fa0d5fdae78359230f892ea8638c4d37e
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-11 16:48:23 +0900

    [D1] Title: 대각선 출력하기, Time: 173 ms, Memory: 58,540 KB -BaekjoonHub

--- SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
@@ -1,2 +1,11 @@
+# 2027. 대각선 출력하기
+# for i in range(5) :
+#     for j in range(5) : 
+#         if j == i : 
+#             print('#', end='')
+#         else : 
+#             print('+', end='')
+#     print()
+
 for i in range(5) :
     print('+'*i, '#', '+'*(4-i), sep='')
