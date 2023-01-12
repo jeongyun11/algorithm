9052dbd92aef00aed571e3e7e470f45245b0574b
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-12 13:29:32 +0900

    [D1] Title: 아주 간단한 계산기, Time: 144 ms, Memory: 58,760 KB -BaekjoonHub

--- SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
@@ -1,9 +1,3 @@
-# 2071
+a,b = map(int, input().split())
 
-a,b = map(int,input().split())
-
-# + - * /
-print(a + b)
-print(a - b)
-print(a * b)
-print(int(a / b))
+print(a+b, a-b, a*b, a//b,sep='\n')
