7075c6084632eb1cd033699980742d97acedfe66
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-17 16:17:40 +0900

    [Bronze I] Title: 더하기 사이클, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,18 +1,17 @@
-num_str = input() # 1
-if int(num_str) < 10 :
-    num_str += '0'
-num_copy = num_str
+# 1110
+num = int(input())
+num_first = num
 count = 0
 
 while True :
-    sum_one = 0
-    for num_one in num_str :
-        sum_one += int(num_one)
-
-    num_str = num_str[-1] + str(sum_one)[-1] # 새로운 수 11
+    num_tenth = (num // 10) + (num % 10)
+    if num_tenth >= 10 : 
+        num_tenth -= 10
+        
+    num = num_tenth + (num % 10) * 10
+    
     count += 1
-
-    if num_str == num_copy :
+    if num == num_first :
         break
 
 print(count)
