7d2c39edf20ea78f3d4ec007fb5fd104190fa811
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-17 16:36:57 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,9 +1,9 @@
 N = int(input())
 
-for n1 in range(1, N + 1) :
-    for n2 in range(n1) :
-        print('*', end='')
-    print()
+# for n1 in range(1, N + 1) :
+#     for n2 in range(n1) :
+#         print('*', end='')
+#     print()
 
-# for n in range(1, N + 1) :
-#     print('*' * n)
+for n in range(1, N + 1) :
+    print('*' * n)
