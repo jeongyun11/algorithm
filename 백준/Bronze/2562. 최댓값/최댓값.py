798903b9228e04458e7f0b97fe47c1ee995d42e5
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-17 21:03:35 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,12 +1,2 @@
-for i in range(9) : 
-    number = int(input())
-    if i == 0 :
-        max_num = number
-        index = 0
-    elif max_num < number :
-        max_num = number
-        index = i
-        
-print(max_num)
-print(index + 1)
-
+a = ([int(input()) for i in range(9)])
+print(max(a), a.index(max(a)) + 1)
