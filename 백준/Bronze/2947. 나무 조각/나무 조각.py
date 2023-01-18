688fdb6fdfdda73d865c0809ffebf504dbb179f3
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-18 21:22:30 +0900

    [Bronze I] Title: 나무 조각, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.py
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -1,10 +1,8 @@
 numbers = list(map(int,input().split()))
-while True :
-    for i in range(len(numbers) - 1) :
-        if numbers[i] > numbers[i + 1] :
-            temp = numbers[i]
-            numbers[i] = numbers[i + 1]
-            numbers[i + 1] = temp
+
+while numbers != sorted(numbers) :
+    for i in range(len(numbers) - 1) : 
+        if numbers[i] > numbers [i + 1] :
+            numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
             print(*numbers)
-    if numbers == sorted(numbers) :
-        break
+
