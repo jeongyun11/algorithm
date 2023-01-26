e8a80945e29c8d3638d8c4a5bb8415bf17bd932f
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-01-26 16:40:03 +0900

    [Bronze II] Title: 벌집, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,15 +1,13 @@
+# import sys
+# sys.stdin = open("test.txt","r")
+
 a = int(input())
 
 i = 1 # 중앙에서 거리
-num_add = 6
-num_start = 0 # i 번째 층의 집의 양
-num_end = 1 # i 번째 층의 집의 양
-
-while True:
-  if num_start < a <= num_end :
-    break
-  num_start = num_end
-  num_end += num_add
+num_add = 6 # 벌집 증가 수
+num = 1 # 벌집 수
+while num < a:
+  num += num_add
   num_add += 6
   i += 1
 
