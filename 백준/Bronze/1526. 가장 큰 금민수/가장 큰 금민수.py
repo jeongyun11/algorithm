7433eef2ef88139ccc81809acf03e15a0c5b6c2c
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-02-01 16:57:54 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -4,17 +4,6 @@ N = input()
 if int(N) >= int('4'*(len(N))) :
     result = '7'*(len(N))
 else : result = '7'*(len(N)-1)
-# if N[0] >= '7' : 
-#     result = '7'*len(N)
-# elif N[0] > '4' : 
-#     result = '4'+'7'*(len(N)-1)
-# elif N[0] == '4' : 
-#     if len(N) >= 2 :
-#         if N[1] < '4' :         
-#             result = '7'*(len(N)-1)
-# else : 
-#     result = '7'*(len(N)-1)
-
 
 i = len(result)-1
 
