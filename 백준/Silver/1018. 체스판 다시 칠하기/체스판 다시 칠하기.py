1f31149e909a5dad8e1acb51aecc2ef175dd38a1
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-02-07 01:24:31 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,26 @@
+# 1018.
+Y, X = map(int, input().split())
+matrix = [input() for y in range(Y)]
+# print(matrix)
+
+min_cnt = 64
+
+for y1 in range(Y-8+1) :
+    for x1 in range(X-8+1) :
+
+        for col1,col2 in [('W','B'), ('B','W')] :
+
+            cnt = 0
+
+            for y2 in range(8) :
+                for x2 in range(8) :
+                    # print(matrix[y2+y1][x2+x1], end='')
+                    if (x2 + y2) % 2 == 0 and matrix[y2+y1][x2+x1] != col1:
+                        cnt += 1
+                    elif (x2 + y2) % 2 == 1 and matrix[y2+y1][x2+x1] != col2:
+                        cnt += 1
+                # print()
+            # print()
+            min_cnt = min(min_cnt, cnt)
+
+print(min_cnt)
