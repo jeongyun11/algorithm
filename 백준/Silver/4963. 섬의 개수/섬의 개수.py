b650b163ded2e29edb0cbffba646778853bace9a
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-02-06 23:37:09 +0900

    [Silver II] Title: 섬의 개수, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,44 +1,27 @@
-import sys
+def find_land(matrix, stack, dxdy) :
+    while stack :
+        y, x = stack.pop()
+        for y1, x1 in dxdy : 
+            y_move = y + y1
+            x_move = x + x1
+            if 0 <= x_move < len(matrix[0]) and 0 <= y_move < len(matrix): # 행렬의 범위를 안 벗어나고
+                if matrix[y_move][x_move] == 1 :# 그 값이 1이면
+                    stack.append((y + y1, x + x1)) # 추가한다.
+        matrix[y][x] = 0
+
 while True :
-    X, Y = map(int, sys.stdin.readline().strip().split())
-    if X == 0 or Y == 0 :
+    X, Y = map(int,input().split())
+
+    if X == Y == 0 :
         break
-    matrix1 = [list(map(int, sys.stdin.readline().strip().split())) for y in range(Y)]
-    # print(matrix1)
 
-    list_land = []
-    dict_land = {}
-    for y in range(Y) :
-        for x in range(X) :
-            if matrix1[y][x] == 1 :
-                list_land.append((x,y))
-                dict_land[(x,y)] = []
-    # print(list_land)
-    # print(dict_land)
-
-    for i in range(len(list_land)) :
-        for j in range(len(list_land)) :
-            if list_land[i] != list_land[j] :
-                if (list_land[i][0] - list_land[j][0]) in [-1, 0, 1] and (list_land[i][1] - list_land[j][1]) in [-1, 0, 1] :
-                    dict_land[list_land[i]].append(list_land[j])
-    # print(dict_land)
-
-    visited = [False] * len(list_land)
-    # print(visited)
-
+    matrix = [list(map(int,input().split())) for y in range(Y)]
     cnt = 0
-    while False in visited :
-        i = visited.index(False)
-        stack = [list_land[i]]
-        visited[i] = True
 
-        while stack :
-            # print(stack)
-            for land in dict_land[stack.pop()] :
-                list_land.index(land)
-                if visited[list_land.index(land)] == False :
-                    stack.append(land)
-                    visited[list_land.index(land)] = True
-        cnt += 1
-    
+    for y in range(Y) :
+        while 1 in matrix[y] :
+            stack = [(y, matrix[y].index(1))]
+            find_land(matrix, stack, [(1,1),(1,0),(1,-1),(0,1),(0,-1),(-1,1),(-1,0),(-1,-1)])
+            cnt +=1
+
     print(cnt)
