6040669f44c194a583a9d6dc5c5b2e5ee821dbf8
jeongyun11 <121421398+jeongyun11@users.noreply.github.com>
2023-02-14 16:28:14 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+i = 2
+result = 1
+
+if min(a,b) == 1 :
+    print(1)
+    print(max(a,b))
+
+else :
+    while i <= min(a, b) :
+        if a % i == 0 and b % i == 0 :
+            a = a//i
+            b = b//i
+            result *= i
+        else : 
+            i += 1
+    print(result)
+
+    i = 2
+    while True :
+        if a % i == 0 :
+            a = a//i
+            result *= i
+        elif b % i == 0 :
+            b = b//i
+            result *= i
+        else :
+            i += 1
+        
+        if a == b == 1 :
+            break
+    print(result)
+            
